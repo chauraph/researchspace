1d10e897be4646850e155ba387d563594c345b9c
chauraph <chauraph@gmail.com>
2024-12-30 23:25:48 +0100

    add feature "Import image from IIIF Manifest" to EX_Digital_Image semantic form

--- src/main/web/api/services/template/functions/StringsFunctions.ts
+++ src/main/web/api/services/template/functions/StringsFunctions.ts
@@ -26,5 +26,47 @@ export const StringsFunctions = {
       separator = ' ';
     }
     return text.split(separator);
-  }
+  },
+  includes: function (string: string, substring: string) {
+    if (!string || !substring) {
+      throw new Error('string or substring may simply not be defined');
+    } else if (typeof string !== 'string' || typeof substring !== 'string') {
+      throw new Error('includes needs a string to search');
+    } else if (string.includes(substring)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  /**
+   * improvement: implement regex replace for higher reusability
+   **/
+  removeEscapeBackslashes(str: string) {
+    return String(str).replace(/\\/g, '');
+  },
+  removeDoubleQuotes(str: string) {
+    return String(str).replace(/(['"])/g, '');
+  },
+  findDoubleQuoteGroup(str: string, pos: number) {
+    if (typeof str !== 'string') {
+      throw new Error('findDoubleQuoteGroup needs a string to find');
+    } 
+    if (str.match(/"([^"]*)"/g))
+      return str.match(/"([^"]*)"/g)[pos];
+    else
+      return null;
+  },
+  sanitizeJsonLikeString(str: string) {
+    // Check if the first character is '[' or '{'
+    if (str[0] === '[' || str[0] === '{') {
+      str = str.slice(1);  // Remove the first character
+    }
+  
+    // Check if the last character is ']' or '}'
+    if (str[str.length - 1] === ']' || str[str.length - 1] === '}') {
+      str = str.slice(0, -1);  // Remove the last character
+    }
+  
+    return str;
+  }  
 };
